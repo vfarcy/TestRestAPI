675724d1107719c403be2c9a62b33783c541dadf
Valéry Farcy <vfarcy@free.fr>
2014-11-11 09:55:44 +0100

    updates

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask.ext.restless import APIManager
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people.db'
+db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+
+app.debug = True
+
+db.create_all()
+
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
+api_manager.create_api(Person, methods=['GET', 'POST', 'DELETE', 'PUT'])
+
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+
+if __name__ == "__main__":
+    app.run()
